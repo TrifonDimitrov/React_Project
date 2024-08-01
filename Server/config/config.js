8e420ba1f3a6bf7f59453f6b246ffccd00cb6363
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-08-01 15:05:05 +0300

    Server configuration

--- Server/config/config.js
+++ Server/config/config.js
@@ -4,7 +4,7 @@ const config = {
     development: {
         port: process.env.PORT || 3000,
         dbURL: 'mongodb://localhost:27017/mydb',
-        origin: ['http://localhost:5555', 'http://localhost:5173']
+        origin: 'http://localhost:5173',
     },
     production: {
         port: process.env.PORT || 3000,
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Променете на 5173 за Vite сървъра
+    host: '0.0.0.0'
+  }
 })
