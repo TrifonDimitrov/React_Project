c561e6ddd192d33647657005246f56a1a48462f2
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-08-03 13:54:00 +0300

    Fetching products

--- src/components/product-list/Products.tsx
+++ src/components/product-list/Products.tsx
@@ -1,18 +1,7 @@
 import React, { useEffect, useState } from "react";
 import * as productsApi from "../../api/products-api";
+import ProductListItem from "./product-list-item/ProductListItem";
 
-// const products = [
-//   {
-//     brand: "",
-//     model: "",
-//     coolingCapacity: "",
-//     heatingCapacity: "",
-//     energyEfficiencyRating: "",
-//     price: "",
-//     description: "",
-//     imageUrl: "",
-//   },
-// ];
 
 export default function productList() {
   const [products, setProduct] = useState([]);
@@ -22,28 +11,12 @@ export default function productList() {
   }, []);
   
   return (
-    
     <div className="bg-white">
       <div className="mx-auto mt-5 max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product._id} className="group">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                <img
-                  alt={product.description}
-                  src={product.imageUrl}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.brand}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {product.price}
-              </p>
-            </div>
-          ))}
-        </div>
+      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {products.map(product => <ProductListItem key={product._id}{...product}/>)}
       </div>
+    </div>
     </div>
   );
 }
---
+++ src/components/product-list/product-list-item/ProductListItem.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+export default function ProductListItem({
+  _id,
+  brand,
+  model,
+  coolingCapacity,
+  heatingCapacity,
+  energyEfficiencyRating,
+  price,
+  description,
+  imageUrl,
+}) {
+  return (
+    <div key={_id} className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <img
+          alt={description}
+          src={imageUrl}
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{brand}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{price}</p>
+      <div className="text-right pr-5">
+      <Link to={`/details/${_id}`} className="text-blue-500 hover:text-blue-700 font-medium">Details</Link>
+      </div>
+    </div>
+  );
+}
