c8f77001c8b7db3aa9dd553fbbf933996b009061
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-08-17 14:18:20 +0300

    Add interface for products

--- src/components/product-list/Products.tsx
+++ src/components/product-list/Products.tsx
@@ -2,10 +2,20 @@ import React, { useEffect, useState } from "react";
 import * as productsApi from "../../api/products-api";
 import ProductListItem from "./product-list-item/ProductListItem";
 
-
+interface Product {
+  _id: string,
+  brand: string,
+  model: string,
+  coolingCapacity: string,
+  heatingCapacity: string,
+  energyEfficiencyRating: string,
+  price: string,
+  description: string,
+  imageUrl: string
+}
 
 export default function productList() {
-  const [products, setProduct] = useState([]);
+  const [products, setProduct] = useState<Product[]>([]);
 
   useEffect(() => {
     productsApi.getAll().then(result => setProduct(result));
@@ -14,7 +24,6 @@ export default function productList() {
   return (
     <div className="bg-white">
     <div className="mx-auto mt-5 max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-      {/* Мрежа с продуктите */}
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {products.map(product => (
           <ProductListItem key={product._id} {...product} />
